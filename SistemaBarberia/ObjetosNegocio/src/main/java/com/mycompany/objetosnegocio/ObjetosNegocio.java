f484370a4a3c890af1038a30a2ad7a9816cdaf89
rodvillarg <jesus.villegas261186@potros.itson.edu.mx>
2026-03-27 13:11:29 -0700

    Crear Proyecto

---
+++ SistemaBarberia/ObjetosNegocio/src/main/java/com/mycompany/objetosnegocio/ObjetosNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.objetosnegocio;
+
+/**
+ *
+ * @author LABCISCO-PC071
+ */
+public class ObjetosNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
